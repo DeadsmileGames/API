f7cff717918e3f06f878c79db833b774e2001359
luxjson <luxjson@gmail.com>
2026-09-12 20:53:03 -0300

    test

--- src/app.js
+++ src/app.js
@@ -141,6 +141,7 @@ export function createApp() {
     const allowedOrigins = [
       env.frontendUrl,
       'http://localhost:5173',
+      'http://localhost:5174',
       'http://localhost:8081',
     ];
 
