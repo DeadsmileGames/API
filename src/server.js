cab18c6f026677537a1295d44e9ee834cd5d39b6
luxjson <luxjson@gmail.com>
2026-09-15 17:32:23 -0300

    fixSecurity

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import { createServer } from 'node:http';
-import app from './app.js';
+import app from './http-app.js';
 import { env } from './config/env.js';
 import { attachRealtimeServer } from './realtime/hub.js';
 
 const server = createServer(app);
+
 attachRealtimeServer(server);
 
 if (!process.env.VERCEL) {
